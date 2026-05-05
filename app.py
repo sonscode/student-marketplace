5f048d4491293cbfee10d5175a01a516ff6a33bf
Sonna <ulrichsonna20@gmail.com>
2026-05-05 03:21:24 +0100

    Added urgency date-time logic -> boost handling

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import re
 from flask import Flask, render_template, request, redirect
+from datetime import datetime
 import sqlite3
 
 app = Flask(__name__)
@@ -19,6 +21,8 @@ def init_db():
             title TEXT,
             price TEXT,
             category TEXT,
+            phone TEXT,
+            leave_date TEXT,       
             is_featured INTEGER DEFAULT 0
             )
     """)
@@ -41,21 +45,46 @@ def home():
         cursor.execute("SELECT * FROM listings ORDER BY is_featured DESC, id DESC")
 
     listings = cursor.fetchall()
+    enhanced_listings = []
+
+    for item in listings:
+        leave_date = datetime.strptime(item['leave_date'], "%Y-%m-%d").date()
+        today = datetime.today().date()
+        days_left = (leave_date - today).days
+
+        enhanced_listings.append({
+            "id": item["id"],
+            "title": item["title"],
+            "price": item["price"],
+            "category": item["category"],
+            "phone": item["phone"],
+            "is_featured": item["is_featured"],
+            "days_left": days_left
+        })
     conn.close()
-    return render_template("index.html", listings=listings)  
+    return render_template("index.html", listings=enhanced_listings)
 
 @app.route('/add', methods=['POST'])
 def add_listing():
     conn=get_db()
     cursor=conn.cursor()
 
+    raw_phone = request.form['phone']
+    phone = re.sub(r'\D', '', raw_phone)
+
+    # If it's a local number (no country code)
+    if len(phone) == 9:
+        phone = '237' + phone
+
     title=request.form['title']
     price=request.form['price']
     category = request.form['category']
+    leave_date = request.form['leave_date']
+    # phone = request.form['phone'].replace('+', '').replace(' ', '')  # Remove + and spaces from phone number
     is_featured = 1 if 'featured' in request.form else 0
 
-    cursor.execute("INSERT INTO listings (title, price, category, is_featured) VALUES (?, ?, ?, ?)", (title, price, category, is_featured))
-    
+    cursor.execute("INSERT INTO listings (title, price, category, phone, leave_date, is_featured) VALUES (?, ?, ?, ?, ?, ?)", (title, price, category, phone, leave_date, is_featured))
+
     conn.commit()
     conn.close()
 
